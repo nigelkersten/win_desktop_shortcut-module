a0a12661b6e22d415658970bcdf27466a3179794
Nigel Kersten <nigel@puppetlabs.com>
2012-03-27 09:48:29 -0700

    Use double-slash in file path to avoid escape warning

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,7 @@ class win_desktop_shortcut {
   if $osfamily == "windows" {
     if $win_common_desktop_directory {
 
-      file { "${win_common_desktop_directory}\PuppetLabs.URL":
+      file { "${win_common_desktop_directory}\\PuppetLabs.URL":
         ensure  => present,
         content => "[InternetShortcut]\nURL=http://puppetlabs.com",
       }
